b7146239dbe39c62099192ce72cd427d877ce2f2
sebastian medina <sebastianlopez_21@hotmail.com>
2023-06-21 11:09:44 -0400

    commit tree fbf4066cb1a5cc1d06ab38a5d002c03429054c02

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,17 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if(params[:name])
+      student_by_first_or_last = Student.where(first_name: params[:name].capitalize).or(Student.where(last_name: params[:name].capitalize))
+      render json: student_by_first_or_last 
+    else
+      students = Student.all
+      render json: students
+    end
   end
 
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
 end
